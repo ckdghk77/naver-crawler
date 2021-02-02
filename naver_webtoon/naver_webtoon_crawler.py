2587584be651e37daf25d971cef5495015fca96b
ckdghk77 <cdkghk777@gmail.com>
2021-02-02 23:09:47 +0900

    ++ Edited result save policy (e.g. "directory structure") ++ only crawling webtoon images (ignore comments) ++ edited cutting policy (more elaborate cut by scene) ++ code is updated for the 2021 Naver webtoon service.

--- naver_webtoon/naver_webtoon_crawler.py
+++ naver_webtoon/naver_webtoon_crawler.py
@@ -2,41 +2,51 @@
 
 """
 Created on Jun 13, 2016
+Edited on Feb 02, 2021
 
 @author: TYchoi
+@edited: CHlee
 
 naver_webtoon_crawler.py allows you to retrieve meta data of webtoon episodes on Naver.
 It automatically updates database by accessing the most current page. 
 Meta data includes: episode title, episode number, date published, length of episode (number of cuts), 
 URL, thumbnail URL, best comments submitted by users, number of recommendations.
+
+
+++ edited save policy ("directory structure")
+++ only crawling webtoons
 """
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
+import os
 import csvHandler as csv
 import urllib.request
+
 import webtoon_cut_counter as cut_counter
 import webtoon_database
+
 
 def get_list_of_webtoon(soup):
     """
     Retrieves most up-to-date webtoon list
 
     Args:
-        soup: BeautifulSoup page source file 
+        soup: BeautifulSoup page source file
     Returns:
         list_of_cartoon : most up-to-date webtoon list
     """
     list_of_cartoon = {}
-    titles = soup.find_all('a',{'class':'title'})
+    titles = soup.find_all('a', {'class': 'title'})
     for comment in titles:
         hyperLink = comment['href']
-        id_finder_front = hyperLink.find('titleId=')+8
+        id_finder_front = hyperLink.find('titleId=') + 8
         id_finder_end = hyperLink.find('&weekday')
-        webtoon_id = hyperLink[id_finder_front : id_finder_end]
+        webtoon_id = hyperLink[id_finder_front: id_finder_end]
         list_of_cartoon[comment.text] = webtoon_id
     return list_of_cartoon
+
 
 def page_source_retriever(driver, search_url):
     """
@@ -44,51 +54,52 @@ def page_source_retriever(driver, search_url):
 
     Args:
         driver : currently active webdriver
-        search_url : a webpage we want to load 
+        search_url : a webpage we want to load
     Returns:
-        soup: BeautifulSoup page source file 
+        soup: BeautifulSoup page source file
     """
 
     driver.get(search_url)
     # change time.sleep to be appropriate to your internet connection.
     time.sleep(1.5)
-    r=driver.page_source
-    soup=BeautifulSoup(r, "lxml") 
+    r = driver.page_source
+    soup = BeautifulSoup(r, "lxml")
     return soup
+
 
 def get_best_comments(new_url_list, best_comment_file, myId, pw):
     """
-    Gathers best comments for each episode of a webtoon which are submitted and evaluated by user  
+    Gathers best comments for each episode of a webtoon which are submitted and evaluated by user
 
     Args:
         new_url_list : a list of URLs to webtoon episodes that we need to add to database
         best_comment_file : a name of file that stores best comments
         myId : your Naver id
-        pw : your Naver password 
+        pw : your Naver password
     Raises:
         NoSuchElementException: 1. Wrong id or password information
-                                2. Naver is not static 
+                                2. Naver is not static
     """
-    
+
     print('Best Comments Downloader Launching....................')
     driver = initialize_webdriver(myId, pw)
-    
+
     completeList = []
-        
+
     for i in range(len(new_url_list)):
         print('-------------------------------------------------------------------------------')
-        print(i+1,new_url_list[i][1],new_url_list[i][2])
-        url = new_url_list[i][5].replace("http://","http://m.")
+        print(i + 1, new_url_list[i][1], new_url_list[i][2])
+        url = new_url_list[i][5].replace("http://", "http://m.")
         episode_link_number = new_url_list[i][3]
         update_date = new_url_list[i][4]
         print(url)
-        soup=page_source_retriever(driver, url)  
-        dates = soup.find_all('div',{'class':'u_comment_info'})
-        comments = soup.find_all('p',{'class':'u_comment_text u_comment_txt1'})
-        likes = soup.find_all('div',{'class':'u_comment_recomm __comment_recomm'})
+        soup = page_source_retriever(driver, url)
+        dates = soup.find_all('div', {'class': 'u_comment_info'})
+        comments = soup.find_all('p', {'class': 'u_comment_text u_comment_txt1'})
+        likes = soup.find_all('div', {'class': 'u_comment_recomm __comment_recomm'})
         for date in range(len(dates)):
-            times = dates[date].text[dates[date].text.find(")")+2:dates[date].text.find(" |")]
-            comment = comments[date].text.replace("BEST","") 
+            times = dates[date].text[dates[date].text.find(")") + 2:dates[date].text.find(" |")]
+            comment = comments[date].text.replace("BEST", "")
             numb_of_likes = likes[date].find_all('em')
             like_counts = []
             if numb_of_likes:
@@ -97,28 +108,31 @@ def get_best_comments(new_url_list, best_comment_file, myId, pw):
             idx = int(new_url_list[i][0])
             name = new_url_list[i][1]
             episode = new_url_list[i][2]
-            list_to_add = [idx, name, episode, episode_link_number, update_date, str(date+1), times, like_counts[0], like_counts[1], comment]
+            list_to_add = [idx, name, episode, episode_link_number, update_date, str(date + 1), times, like_counts[0],
+                           like_counts[1], comment]
             print(list_to_add)
-            completeList.append(list_to_add)            
+            completeList.append(list_to_add)
     driver.close()
     csv.csvWriter(completeList, best_comment_file)
     print('Done with Best Comments Downloader !!!!')
+
 
 def get_currently_publishing_webtoon(driver):
     """
     Creates most up-to-date webtoon list
 
     Args:
-        driver: a webdriver object  
+        driver: a webdriver object
     Returns:
         current_webtoon_titles : most up-to-date webtoon list
     """
-    
+
     url = 'http://comic.naver.com/webtoon/weekday.nhn'
     soup = page_source_retriever(driver, url)
     current_webtoon_titles = get_list_of_webtoon(soup)
-    
+
     return current_webtoon_titles
+
 
 def initialize_webdriver(myId, pw):
     """
@@ -127,7 +141,7 @@ def initialize_webdriver(myId, pw):
 
     Args:
         myId : your Naver id
-        pw : your Naver password 
+        pw : your Naver password
     Returns:
         driver : a logged-in webdriver
     Raises:
@@ -135,7 +149,7 @@ def initialize_webdriver(myId, pw):
     """
 
     baseurl = 'https://nid.naver.com/nidlogin.login'
-    xpaths = {'id': "//input[@name='id']", 'pw':"//input[@name='pw']"}
+    xpaths = {'id': "//input[@name='id']", 'pw': "//input[@name='pw']"}
     driver = webdriver.Chrome()
 
     driver.get(baseurl)
@@ -144,8 +158,9 @@ def initialize_webdriver(myId, pw):
     btn = driver.find_element_by_css_selector('.int_jogin')
     btn.click()
     driver.implicitly_wait(10)
-    
+
     return driver
+
 
 def url_builder(webtoon_id, episode_num):
     """
@@ -153,9 +168,9 @@ def url_builder(webtoon_id, episode_num):
 
     Args:
         webtoon_id: a webtoon id
-        episode_num : a webtoon episode number 
+        episode_num : a webtoon episode number
     Returns:
-        url : a url to an appropriate webtoon episode  
+        url : a url to an appropriate webtoon episode
     Raises:
         AttributeError : a query must be String
     """
@@ -163,8 +178,9 @@ def url_builder(webtoon_id, episode_num):
     if episode_num == '':
         url = 'http://comic.naver.com/webtoon/list.nhn?titleId={}'.format(webtoon_id)
     else:
-        url = 'http://comic.naver.com/webtoon/detail.nhn?titleId={0}&no={1}'.format(webtoon_id,episode_num)
+        url = 'http://comic.naver.com/webtoon/detail.nhn?titleId={0}&no={1}'.format(webtoon_id, episode_num)
     return url
+
 
 def check_updates(webtoon_title_file, phantom_path, chrome_path):
     """
@@ -176,41 +192,41 @@ def check_updates(webtoon_title_file, phantom_path, chrome_path):
         phantom_path : the executable PhantomJS path
         chrome_path : the executable Chrome driver path
     Returns:
-        saved_webtoon : a list of ids of webtoons that are newly added 
+        saved_webtoon : a list of ids of webtoons that are newly added
         deleted_list :  a list of ids of webtoons that have been deleted from database
     """
-    
+
     driver = webdriver.PhantomJS(executable_path=phantom_path)
     current_webtoon = get_currently_publishing_webtoon(driver)
     driver.close()
 
     try:
         saved_webtoon = csv.csvReader_toDict(webtoon_title_file)
-        print("sucessfully loaded", webtoon_title_file,'.csv') 
+        print("sucessfully loaded", webtoon_title_file, '.csv')
     except:
         saved_webtoon = {}
-        
+
     current_webtoon_keys = list(current_webtoon)
     saved_webtoon_keys = list(saved_webtoon)
-    print (len(current_webtoon_keys), len(saved_webtoon_keys))
+    print(len(current_webtoon_keys), len(saved_webtoon_keys))
     number_of_changes = 0
-    
+
     added_list = []
     deleted_list = []
-    
+
     for item in current_webtoon_keys:
         if item not in saved_webtoon_keys:
             saved_webtoon[item] = current_webtoon[item]
             added_list.append(current_webtoon[item])
             number_of_changes += 1
-    
+
     for item in saved_webtoon_keys:
         if item not in current_webtoon_keys:
             deleted_list.append(saved_webtoon[item])
             del saved_webtoon[item]
             number_of_changes += 1
-    
-    if number_of_changes == 0 :
+
+    if number_of_changes == 0:
         print("No Webtoon List Updates Required")
     else:
         webtoon_database.execute(chrome_path)
@@ -218,27 +234,29 @@ def check_updates(webtoon_title_file, phantom_path, chrome_path):
         if len(deleted_list) == 0:
             deleted_list.append("NONE")
         elif len(added_list) == 0:
-            added_list.append("NONE")    
-        print("Added:",", ".join(added_list),"Deleted:",", ".join(deleted_list))
+            added_list.append("NONE")
+        print("Added:", ", ".join(added_list), "Deleted:", ", ".join(deleted_list))
         csv.csvDictWriter(saved_webtoon, webtoon_title_file)
-    
+
     return saved_webtoon, deleted_list
+
 
 def get_dates(soup):
     """
     Retrieves dates that webtoon episodes are published
 
     Args:
-        soup: BeautifulSoup page source file 
+        soup: BeautifulSoup page source file
     Returns:
         dates : a list of dates
     """
-    
+
     dates = []
-    date_list = soup.find_all('td',{'class':'num'})
+    date_list = soup.find_all('td', {'class': 'num'})
     for date in date_list:
-        dates.append(date.text.replace(".",""))
+        dates.append(date.text.replace(".", ""))
     return dates
+
 
 def get_new_thumbnail_episode_url(webtoon_list, url_database, phantom_path, chrome_path):
     """
@@ -253,10 +271,10 @@ def get_new_thumbnail_episode_url(webtoon_list, url_database, phantom_path, chro
     Returns:
         new_list : a crawl queue to webtoon episode and its thumbnail image
     """
-    
+
     driver = webdriver.PhantomJS(executable_path=phantom_path)
     driver2 = webdriver.Chrome(chrome_path)
-    
+
     new_list = []
     for name in sorted(webtoon_list.keys()):
         print('-----------------------------------')
@@ -270,25 +288,34 @@ def get_new_thumbnail_episode_url(webtoon_list, url_database, phantom_path, chro
             num_count = 0
             dates = get_dates(soup)
             last_episode_num = soup.find('tbody').find_all('td')
-            recom_num = soup.find('em',{'class':'u_cnt'}).text
-            duplicates_appeared = False            
+            recom_num = soup.find('em', {'class': 'u_cnt'}).text
+            duplicates_appeared = False
             for each in last_episode_num:
                 list_of_images = each.find_all('img')
                 for item in list_of_images:
                     if 'title' in str(item) and '배경음악 있음' not in item['title']:
                         thumbnail_link = item['src']
-                        episode_num_finder_1 = thumbnail_link.find(webtoon_list[name])+len(webtoon_list[name])+1
-                        episodeNmeFinder2 = thumbnail_link.find('/inst_thumbnail')
-                        episodeLinkNumber = thumbnail_link[episode_num_finder_1:episodeNmeFinder2]
+                        episode_num_finder_1 = thumbnail_link.find(webtoon_list[name]) + len(webtoon_list[name]) + 1
+                        episodeNmeFinder2 = thumbnail_link[episode_num_finder_1:].find('/')
+                        episodeLinkNumber = thumbnail_link[
+                                            episode_num_finder_1:episode_num_finder_1 + episodeNmeFinder2]
                         url = url_builder(webtoon_list[name], episodeLinkNumber)
-                        cut_counts = cut_counter.count_cuts(url.replace("http://","http://m."),driver2, webtoon_list[name], episodeLinkNumber)
-                        list_to_add = [webtoon_list[name], recom_num,name,item['title'],episodeLinkNumber,dates[num_count], cut_counts,url,thumbnail_link]
+
+                        folder_name = os.path.join("./splitted_images",
+                                                   webtoon_list[name] + "_" + str(episodeLinkNumber));
+                        if os.path.exists(folder_name):
+                            continue;
+
+                        cut_counts = cut_counter.count_cuts(url.replace("http://", "http://m."), driver2,
+                                                            webtoon_list[name], episodeLinkNumber)
+                        list_to_add = [webtoon_list[name], recom_num, name, item['title'], episodeLinkNumber,
+                                       dates[num_count], cut_counts, url, thumbnail_link]
                         if list_to_add not in url_database:
                             print(list_to_add)
                             new_list.append(list_to_add)
-                            num_count+=1
+                            num_count += 1
                         else:
-                            num_count+=1
+                            num_count += 1
                             next_button_exists = False
                             duplicates_appeared = True
                 if duplicates_appeared:
@@ -296,13 +323,13 @@ def get_new_thumbnail_episode_url(webtoon_list, url_database, phantom_path, chro
                 else:
                     pass
             if next_button_exists:
-                try :
+                try:
                     next_button = driver.find_element_by_css_selector('a.next')
                     next_button.click()
                     url = driver.current_url
                     time.sleep(0.5)
                 except:
-                    next_button_exists=False
+                    next_button_exists = False
             else:
                 break
 
@@ -310,50 +337,53 @@ def get_new_thumbnail_episode_url(webtoon_list, url_database, phantom_path, chro
     driver2.close()
     return new_list
 
+
 def thumbnail_downloader(new_urllist):
     """
-    Downloads thumbnail images 
+    Downloads thumbnail images
 
     Args:
         new_urllist: a crawl queue to webtoon episode and its thumbnail image
-    """    
-    
+    """
+
     print("downloading thumbnails................")
     counter = 1
     for url in new_urllist:
         try:
-            urllib.request.urlretrieve(url[-1], "thumbnail/"+url[0]+"_"+url[4]+"_"+url[3]+".jpg")
-            print("Done With ",counter,"out of ",len(new_urllist))
+            urllib.request.urlretrieve(url[-1], "thumbnail/" + url[0] + "_" + url[4] + "_" + url[3] + ".jpg")
+            print("Done With ", counter, "out of ", len(new_urllist))
             counter += 1
         except:
-            print('we have a problem with !!'+url[-1])
+            print('we have a problem with !!' + url[-1])
             counter += 1
     print("done downloading thumbnails!!!")
-    
+
+
 def database_update(deleted_list, webtoon_url_file):
     """
     Delete Webtoon episodes that are completed publishing
 
     Args:
         deleted_list: a list of webtoons that are completed
-        webtoon_url_file : un-updated URL database 
+        webtoon_url_file : un-updated URL database
     Returns:
         url_database : a updated URL databse
     """
 
-    # if we don't have url database created, we make one.  
+    # if we don't have url database created, we make one.
     try:
         url_database = csv.csvReader_toList(webtoon_url_file)
-        print("sucessfully loaded", webtoon_url_file,'.csv')        
+        print("sucessfully loaded", webtoon_url_file, '.csv')
     except:
-        url_database = []       
-    
+        url_database = []
+
     if deleted_list:
-        print('deleting',", ".join(deleted_list),"from",webtoon_url_file)
+        print('deleting', ", ".join(deleted_list), "from", webtoon_url_file)
         for item in url_database:
             if item[0] in deleted_list:
                 url_database.remove(item)
     return url_database
+
 
 def main():
     """
@@ -363,34 +393,31 @@ def main():
     3. checks if there are new episodes. Returns urls of thumbnails and episodes that we need to add to database.
     4. download thumbnail images
     5. update our url database
-    6. retrieve best comments for each episode 
+    6. retrieve best comments for each episode
     Raises:
         selenium.common.exceptions.WebDriverException: the webdriver cannot be found
-    """    
-    
-    myId = ''
-    pw = ''
+    """
+
     phantom_path = ''
-    chrome_path = ''
-    
-    webtoon_url_file = ''
-    webtoon_title_file = ''
-    best_comment_file = ''
-    
-    webToonList, deletedList = check_updates(webtoon_title_file, phantom_path, chrome_path) 
+    chrome_path = ""
+
+    webtoon_url_file = './webToonUrls'
+    webtoon_title_file = './webToonTitles'
+
+    webToonList, deletedList = check_updates(webtoon_title_file, phantom_path, chrome_path)
     url_database = database_update(deletedList, webtoon_url_file)
-     
- 
+
     new_url_list = get_new_thumbnail_episode_url(webToonList, url_database, phantom_path, chrome_path)
- 
-    if len(new_url_list) > 0 :
+
+    if len(new_url_list) > 0:
         thumbnail_downloader(new_url_list)
         url_database = url_database + new_url_list
         csv.csvWriter(url_database, webtoon_url_file)
-        get_best_comments(new_url_list, best_comment_file, myId, pw)    
-     
+        # get_best_comments(new_url_list, best_comment_file, myId, pw)
+
     else:
         print("No Webtoon To Update.")
-     
+
+
 if __name__ == "__main__":
     main()
--- naver_webtoon/webtoon_cut_counter.py
+++ naver_webtoon/webtoon_cut_counter.py
@@ -1,85 +1,76 @@
 """
 Created on Jul 29, 2016
+Edited on Feb 02, 2021
 
 @author: TYchoi
+@edited by : CHLee
 
 webtoon_cut_counter.py counts the number of cuts that a webtoon episode contains.
 This information can be used as the "length" information of each episode.
+
+
+++ More elaborate cutting (cut by scene)
+
 """
 from PIL import Image
 from selenium import webdriver
 import urllib.request
+import numpy as np
 import time
+import os
 
 cut_num = 0
 
-def image_loader(name):
+def define_non_white_region(total_img):
     """
-    Loads a downloaded image
+    Defines non-white region : images locations
 
-    Args:
-        name: the name of image we want to load 
-    Returns:
-        height_length : the height of the image
-        height_pix_list : the list of height indices that contains all white rows
-    """
-    
-    im = Image.open(name)
-    pix = im.load()       
-    width_length, height_length = im.size
-    height_pix_list = []
-    for height in range(height_length):
-        width_pix_list1 = []
-        width_pix_list2 = []
-        contains_non_white = False
-        for width in range(width_length):
-            try:
-                r,g,b = pix[width,height]
-            except:
-                r,g,b,a = pix[width,height]
-            if r > 240 and g > 240 and b >240:
-                width_pix_list1.append(width)
-            else:
-                contains_non_white = True
-            if contains_non_white:
-                break
-        if not contains_non_white:
-            height_pix_list.append(height)
-                
-    return height_length, height_pix_list
-
-def define_non_white_region(height_length, height_pix_list):
-    """
-    Defines non-white region : images locations 
-    
     Args:
         height_length : the height of the image
         height_pix_list : the list of height indices that contains all white rows
     Returns:
         white_region : a list of height indices, where a image starts or ends
     """
-        
-    white_region = [0]
-    for i in range(len(height_pix_list)-1):
-        if height_pix_list[i+1]-height_pix_list[i] > 2:
-            if height_pix_list[i] not in white_region:
-                white_region.append(height_pix_list[i])
-                white_region.append(height_pix_list[i+1])
-    if len(white_region) % 2 == 1:
-        white_region.append(height_pix_list[-1])
-        
-    if white_region[-1] == height_length-1:
-        footer_ends_at = white_region[-2]
-        white_region.remove(white_region[-1])
-        white_region.remove(footer_ends_at)
 
-    return white_region
+    width_length, height_length = total_img.size
+
+    total_pix = np.transpose(np.asarray(total_img),(2,1,0));
+
+
+    is_pointer_background = True;
+    is_scanning = False;
+    start_height_list = []
+    end_height_list = []
+
+    for height in range(height_length):
+
+        line_pix = total_pix[:,:, height];
+
+        uniq_num = np.unique(line_pix, axis=1).shape[1];
+
+        if uniq_num < 3 :  # black and white line # or simple line
+            if is_pointer_background==False and is_scanning == True :
+                ## background start
+                end_height_list.append(height);
+                is_scanning = False;
+            is_pointer_background = True;
+        else :
+            if is_pointer_background == True and is_scanning == False :
+                ## background_ end
+                start_height_list.append(height);
+                is_scanning = True
+
+            is_pointer_background = False;
+
+    if len(end_height_list) != len(start_height_list) :
+        end_height_list.append(height_length-1);
+    return start_height_list, end_height_list
 
 
 def count_cuts(url, driver, webtoon_id, episode_number):
     """
-    Counts the number of cuts (frames) within an episode 
-    
+    Counts the number of cuts (frames) within an episode
+
     Args:
         url : an URL to a webtoon episode
         driver : a initialized webdriver
@@ -90,50 +81,86 @@ def count_cuts(url, driver, webtoon_id, episode_number):
     """
 
     driver.get(url)
-    image_blocks = driver.find_element_by_css_selector('#toonLayer').find_elements_by_tag_name('img')
-    sample_url = image_blocks[0].get_attribute('src').replace('001.jpg','')
+    try :
+        image_blocks = driver.find_element_by_css_selector('#toonLayer').find_elements_by_tag_name('img')
+        sample_url = image_blocks[0].get_attribute('src').replace('001.jpg','')
+    except :
+        return 0
+
     number_of_cuts = 0
-    
-    for i in range(1,len(image_blocks)-1):
+
+    im_total = None;
+
+    for i in range(1,len(image_blocks)):
+        time.sleep(np.random.uniform(low=0.0, high=1.0));
+
         save_name = 'cutCounter/'+str(i)+'.jpg'
         number = '00' + str(i)
         if len(number) > 3:
             number = number[1:]
         img_url = sample_url+number+'.jpg'
-        urllib.request.urlretrieve(img_url, save_name)
-        height_length, height_pix_list = image_loader(save_name)
+        #reqUrl = urllib.request.Request(img_url, headers={'User-Agent': 'Mozilla/5.0'});
+
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        urllib.request.install_opener(opener)
+
         try :
-            white_region = define_non_white_region(height_length, height_pix_list)
-            number_of_cuts += crop_image(white_region, save_name, webtoon_id, episode_number)
+            urllib.request.urlretrieve(img_url, save_name)
         except:
-            number_of_cuts += 0
-        break
+            break;
+
+        im = Image.open(save_name);
+
+        if im_total is None:
+            im_total = im;
+        else:
+            im_new = Image.new('RGB', (im_total.width, im_total.height + im.height));
+            im_new.paste(im_total, (0,0));
+            im_new.paste(im, (0,im_total.height));
+            im_total = im_new
+
+    non_white_start, non_white_end  = define_non_white_region(im_total)
+    number_of_cuts += crop_image(im_total, non_white_start, non_white_end , webtoon_id, episode_number)
 
     return number_of_cuts
 
-def crop_image(white_region, save_name,webtoon_id, episode_number):
+def crop_image(total_img, non_white_start, non_white_end, webtoon_id, episode_number):
     """
-    Crops an large image that contains multiple cuts into individual cuts. 
-    
+    Crops an large image that contains multiple cuts into individual cuts.
+
     Args:
-        white_region : a list of height indices, where a image starts or ends
+        non_white_start : a list of height indices, where a image starts
+        non_white_end : a list of height indices, where a image ends
         save_name : the name of an image file
         webtoon_id : a webtoon id
         episode_number : a webtoon episode number
     Returns:
         cut_num : the number of cuts that an image file has
     """
-    
+
     global cut_num
-    im2 = Image.open(save_name)
-    pix = im2.load()       
-    width_length2, height_length2 = im2.size
-    current_pos = 1
-            
-    for i in range(1,len(white_region)-1):
-        if white_region[i+1] - white_region[i] > 100:
-            im2.crop((0,white_region[i],width_length2,white_region[i+1])).save('splitted_images/'+webtoon_id+str(episode_number)+str(cut_num)+'.jpg')
+
+    width_length, height_length = total_img.size
+    folder_name = os.path.join("./splitted_images", webtoon_id + "_" + str(episode_number));
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+
+    for i in range(len(non_white_start)) :
+
+        if non_white_end[i] - non_white_start[i] > 100:
+            img_out = total_img.crop((0, non_white_start[i], width_length, non_white_end[i]));
+
+            uniq_rate = np.unique(np.reshape(np.transpose(np.array(img_out)), [3, np.prod(img_out.size[-2:])]), axis=1).shape[
+                1] / np.prod(img_out.size[-2:]);
+
+            if uniq_rate < 0.1:
+                continue;
+
+            img_out.save(os.path.join(folder_name,str(cut_num)+'.jpg'))
             time.sleep(0.5)
             cut_num += 1
+
+
     return cut_num
             
