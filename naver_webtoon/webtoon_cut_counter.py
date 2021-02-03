ab87a12511f5c564ae8e9000ff42906bb404bd60
ckdghk77 <cdkghk777@gmail.com>
2021-02-04 01:38:16 +0900

    modifid cutting policy

--- naver_webtoon/webtoon_cut_counter.py
+++ naver_webtoon/webtoon_cut_counter.py
@@ -1,23 +1,24 @@
 """
 Created on Jul 29, 2016
-Edited on Feb 02, 2021
+Edited on Feb 02, 2021 (by ckdghk777@gmail.com)
 
 @author: TYchoi
-@edited by : CHLee
+@edit : ckdghk77
 
 webtoon_cut_counter.py counts the number of cuts that a webtoon episode contains.
 This information can be used as the "length" information of each episode.
 
-
-++ More elaborate cutting (cut by scene)
+1) Fixed minor bugs (connection related bugs)
+2) Modified cutting policy (more elaborate cut)
 
 """
-from PIL import Image
+from PIL import Image, ImageFilter, ImageDraw
 from selenium import webdriver
 import urllib.request
 import numpy as np
 import time
 import os
+import matplotlib.pyplot as plt
 
 cut_num = 0
 
@@ -34,33 +35,32 @@ def define_non_white_region(total_img):
 
     width_length, height_length = total_img.size
 
-    total_pix = np.transpose(np.asarray(total_img),(2,1,0));
+    edge_img = total_img.convert('L').filter(ImageFilter.FIND_EDGES);
 
+    edge_pix = np.transpose(np.asarray(edge_img))/255.0;
 
-    is_pointer_background = True;
+    edge_pix[np.where(edge_pix < 0.1)] = 0.0;
+    edge_pix[np.where(edge_pix >= 0.1)] = 1.0;
+
     is_scanning = False;
     start_height_list = []
     end_height_list = []
 
-    for height in range(height_length):
+    for height in range(1, height_length-1) :
 
-        line_pix = total_pix[:,:, height];
+        edge_h_line = edge_pix[1:-1, height:min(height+30,height_length-1)];
 
-        uniq_num = np.unique(line_pix, axis=1).shape[1];
-
-        if uniq_num < 3 :  # black and white line # or simple line
-            if is_pointer_background==False and is_scanning == True :
+        if np.sum(edge_h_line) == 0.0 :  # no edge detected + high color consistency
+            if is_scanning == True :
                 ## background start
                 end_height_list.append(height);
                 is_scanning = False;
-            is_pointer_background = True;
-        else :
-            if is_pointer_background == True and is_scanning == False :
+
+        elif np.sum(edge_h_line) > 0.0 :
+            if is_scanning == False :
                 ## background_ end
                 start_height_list.append(height);
                 is_scanning = True
-
-            is_pointer_background = False;
 
     if len(end_height_list) != len(start_height_list) :
         end_height_list.append(height_length-1);
@@ -80,10 +80,15 @@ def count_cuts(url, driver, webtoon_id, episode_number):
         number_of_cuts : the number of cuts that an episode has
     """
 
+    driver.set_page_load_timeout(30)
     driver.get(url)
     try :
         image_blocks = driver.find_element_by_css_selector('#toonLayer').find_elements_by_tag_name('img')
-        sample_url = image_blocks[0].get_attribute('src').replace('001.jpg','')
+        for img_b in image_blocks :
+            sample_url = img_b.get_attribute('src')
+            if "001.jpg" in sample_url :
+                sample_url = sample_url.replace('001.jpg','')
+                break;
     except :
         return 0
 
@@ -103,20 +108,21 @@ def count_cuts(url, driver, webtoon_id, episode_number):
 
         opener = urllib.request.build_opener()
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+
         urllib.request.install_opener(opener)
 
         retry_no = 30;
 
-        while retry_no > 0:  # re-try until success
+        while retry_no > 0 :  # re-try until success
 
-            try:
+            try :
                 urllib.request.urlretrieve(img_url, save_name)
                 break;
             except:
-                retry_no -= 1;
+                retry_no-=1;
                 time.sleep(np.random.uniform(low=0.5, high=3.0))
 
-        if retry_no <= 0:
+        if retry_no <= 0 :
             break;
 
         im = Image.open(save_name);
@@ -157,13 +163,16 @@ def crop_image(total_img, non_white_start, non_white_end, webtoon_id, episode_nu
 
     for i in range(len(non_white_start)) :
 
-        if non_white_end[i] - non_white_start[i] > 100:
+
+        if non_white_end[i] - non_white_start[i] > 200:
             img_out = total_img.crop((0, non_white_start[i], width_length, non_white_end[i]));
 
-            uniq_rate = np.unique(np.reshape(np.transpose(np.array(img_out)), [3, np.prod(img_out.size[-2:])]), axis=1).shape[
-                1] / np.prod(img_out.size[-2:]);
+            unique_color = np.unique(np.reshape(np.transpose(np.array(img_out)), [3, np.prod(img_out.size[-2:])]), axis=1).shape[
+                1];
 
-            if uniq_rate < 0.1:
+            uniq_rate = unique_color  / np.prod(img_out.size[-2:]);
+
+            if uniq_rate < 0.05 or unique_color < 3:
                 continue;
 
             img_out.save(os.path.join(folder_name,str(cut_num)+'.jpg'))
